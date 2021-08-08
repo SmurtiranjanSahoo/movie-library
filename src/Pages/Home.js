34b1ed55dda4893768e9d7350e0ba8fc1462d512
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-08-08 22:46:37 +0530

    feat: add home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { createRef } from "react";
+import HomeSlider from "../Components/HomeSlider/HomeSlider";
 import Navbar from "../Components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div className="App">
       <Navbar />
+      <div style={{ marginTop: "100px" }}>
+        <HomeSlider />
+        <HomeSlider />
+        <HomeSlider />
+        <HomeSlider />
+        <HomeSlider />
+      </div>
     </div>
   );
 };
