401b59066241624aae2a627a57966858d8d5b94b
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-08-08 22:49:08 +0530

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import DetailView from "./Pages/DetailView";
+import PageNotFound from "./Pages/PageNotFound";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/:id" component={DetailView} />
+        <Route path="*" component={PageNotFound} />
       </Switch>
     </Router>
   );
