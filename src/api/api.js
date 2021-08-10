c227d03b63d213f301fd604c61e24533c1108475
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-08-10 22:55:07 +0530

    base api calls

---
+++ src/api/api.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+export const base_url = axios.create({
+  baseURL: `https://api.themoviedb.org/3/`,
+  params: {
+    api_key: process.env.REACT_APP_API_KEY,
+  },
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export const movie_url = (id) =>
+  axios.create({
+    baseURL: `https://api.themoviedb.org/3/movie/${id}?language=en-US`,
+    params: {
+      api_key: process.env.REACT_APP_API_KEY,
+    },
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+export const movie_cast_url = (id) =>
+  axios.create({
+    baseURL: `https://api.themoviedb.org/3/movie/${id}/credits?language=en-US`,
+    params: {
+      api_key: process.env.REACT_APP_API_KEY,
+    },
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+export const tvshow_url = (id) =>
+  axios.create({
+    baseURL: `https://api.themoviedb.org/3/tv/${id}?language=en-US`,
+    params: {
+      api_key: process.env.REACT_APP_API_KEY,
+    },
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+export const tvshow_cast_url = (id) =>
+  axios.create({
+    baseURL: `https://api.themoviedb.org/3/tv/${id}/credits?language=en-US`,
+    params: {
+      api_key: process.env.REACT_APP_API_KEY,
+    },
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
