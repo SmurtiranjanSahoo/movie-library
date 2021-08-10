f18922032dd59309b3d64e521472ade7fa80ecde
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-08-10 22:57:19 +0530

    route setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//components
 import Home from "./Pages/Home";
 import DetailView from "./Pages/DetailView";
 import PageNotFound from "./Pages/PageNotFound";
+import TvShows from "./Pages/TvShows";
+import Movies from "./Pages/Movies";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/:id" component={DetailView} />
+        <Route exact path="/tvshows" component={TvShows} />
+        <Route exact path="/movies" component={Movies} />
+        <Route exact path="/movie/:movieId" component={DetailView} />
+        <Route exact path="/tvshow/:tvshowId" component={DetailView} />
         <Route path="*" component={PageNotFound} />
       </Switch>
     </Router>
   );
-}
+};
 
 export default App;
